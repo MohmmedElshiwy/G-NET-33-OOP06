3b1baa1565415ea1193277275ac12601c7630135
MohmmedElshiwy <mohammed6elshiwy@gmail.com>
2026-03-23 11:18:15 +0300

    Solve Problem 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace G_NET_33_OOP06;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        #region Part 1
+
+        // #region Problem 1
+        //Q1 : What is abstraction in OOP? How is it different from encapsulation? Give a real-world example (not from the session) that shows the difference between the two.
+
+
+        // abstraction is about hiding implementation details
+        //encapsulation is about hiding data and controlling access to it
+
+
+        /*  Different 
+        
+        1- Abstraction
+
+        A) Hides implementation details
+        B) Focuses on what the object does
+        c) Achieved using interfaces / abstract classes
+
+
+        2- Achieved using interfaces / abstract classes
+        A) Hides and protects data
+        B) Focuses on how to control access to data
+        C) Achieved using access modifiers (private, public, etc.)
+        
+        */
+
+
+        /*  Example 
+        
+        Abstraction
+
+        When you use a smartphone:
+
+        You tap an app → it opens
+        You make a call → it connects
+
+        You don’t know how the OS, CPU, or network works internally
+        This is Abstraction (you use features without knowing implementation)
+
+
+
+        Encapsulation:
+
+        Inside the smartphone:
+
+        Internal components (battery data, memory, system files) are hidden
+        You cannot directly access or modify them
+
+        You interact through settings or apps only
+        This is Encapsulation (data is protected and controlled
+        
+        
+        */
+        // #endregion
+
+        #endregion
+    }
+}
